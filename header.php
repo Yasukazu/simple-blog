50004241d0b979850e43cb17c432b923758de9d2
Makino,Yasukazu <mkn@yskz.dev>
2025-06-09 18:31:22 +0900

    Trumbowyg 2.27.3

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ if (!empty(SITE_ROOT)){
 
     <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/ui/trumbowyg.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/ui/trumbowyg.min.css">
 </head>
 <body>
 
